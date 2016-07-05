4225003bd73faa75d353bdd9dc48ed2921c2cca2
Artsiom <Artsiomxxx@gmail.com>
2016-07-06 02:28:12 +0300

    create project

---
+++ GCD/AlgorithmGCD.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace GCD
+{
+    public static class AlgorithmGCD
+    {
+        /// <summary>
+        /// This method is the GCD of two numbers and displays the run-time program.
+        /// </summary>
+        /// <param name="runTime">the run-time program.</param>
+        /// <returns>GCD of numbers./returns>
+        public static int GetGCD(out long runTime, int firstNumber, int secondNumber)
+        {
+            int result = 0;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            result = GCD(firstNumber, secondNumber);
+            stopWatch.Stop();
+            runTime = stopWatch.ElapsedTicks;
+            return result;
+
+        }
+        /// <summary>
+        /// This method is the GCD of two numbers binary and displays the run-time program.
+        /// </summary>
+        /// <param name="runTime">the run-time program.</param>
+        /// <returns>GCD of numbers./returns>
+        public static int GetSteinGCD(out long runTime, int firstNumber, int secondNumber)
+        {
+            int result = 0;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            result = SteinGCD(firstNumber, secondNumber);
+            stopWatch.Stop();
+            runTime = stopWatch.ElapsedTicks;
+            return result;
+
+        }
+        /// <summary>
+        /// This method is the GCD of three numbers and displays the run-time program.
+        /// </summary>
+        /// <param name="runTime">the run-time program.</param>
+        /// <returns>GCD of numbers./returns>
+        public static int GetGCD(out long runTime, int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int result = 0;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            firstNumber = GCD(firstNumber, secondNumber);
+            result = GCD(firstNumber, thirdNumber);
+            stopWatch.Stop();
+            runTime = stopWatch.ElapsedTicks;
+            return result;
+        }
+        /// <summary>
+        /// This method is the GCD of three numbers binary and displays the run-time program.
+        /// </summary>
+        /// <param name="runTime">the run-time program.</param>
+        /// <returns>GCD of numbers./returns>
+        public static int GetSteinGCD(out long runTime, int firstNumber, int secondNumber, int thirdNumber)
+        {
+            int result = 0;
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            firstNumber = SteinGCD(firstNumber, secondNumber);
+            result = SteinGCD(firstNumber, thirdNumber);
+            stopWatch.Stop();
+            runTime = stopWatch.ElapsedTicks;
+            return result;
+        }
+        /// <summary>
+        /// This method is the GCD of numbers and displays the run-time program.
+        /// </summary>
+        /// <param name="runTime">the run-time program.</param>
+        /// <returns>GCD of numbers./returns>
+        public static int GetGCD(out long runTime, params int[] arrayOfNumbers)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int result = arrayOfNumbers[0];
+            for (int i = 0; i < arrayOfNumbers.Length - 1; i++)
+            {
+                result = GCD(result, arrayOfNumbers[i++]);
+            }
+            stopWatch.Stop();
+            runTime = stopWatch.ElapsedTicks;
+            return result;
+        }
+        /// <summary>
+        /// This method is the GCD of numbers binary and displays the run-time program.
+        /// </summary>
+        /// <param name="runTime">the run-time program.</param>
+        /// <returns>GCD of numbers./returns>
+        public static int GetSteinGCD(out long runTime, params int[] arrayOfNumbers)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            int result = arrayOfNumbers[0];
+            for (int i = 0; i < arrayOfNumbers.Length - 1; i++)
+            {
+                result = SteinGCD(result, arrayOfNumbers[i++]);
+            }
+            stopWatch.Stop();
+            runTime = stopWatch.ElapsedTicks;
+            return result;
+        }
+
+        private static int GCD(int firstNumber, int secondNumber)
+        {
+
+            if (firstNumber == secondNumber)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber == 1 || secondNumber == 1)
+            {
+                return 1;
+            }
+
+            if (firstNumber == 0 && secondNumber == 0)
+            {
+                throw new Exception("Invalid function arguments!");
+            }
+
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+
+            if (secondNumber == 0)
+            {
+                return firstNumber;
+            }
+
+            if (firstNumber % secondNumber != 0)
+            {
+                return GCD(secondNumber, firstNumber % secondNumber);
+            }
+            
+            return Math.Abs(secondNumber);
+        }
+
+        private static int SteinGCD(int firstNumber, int secondNumber)
+        {
+            
+            if (firstNumber == secondNumber)
+            {
+                return firstNumber;
+            }
+            if (firstNumber == 1 || secondNumber == 1)
+            {
+                return 1;
+            }
+
+            if (firstNumber == 0 && secondNumber == 0) throw new Exception("Invalid function arguments!");
+
+            if (firstNumber == 0)
+            {
+                return secondNumber;
+            }
+
+            if (secondNumber == 0)
+            {
+                return firstNumber;
+            }
+            if ((firstNumber % 2 == 0) && (secondNumber % 2 == 0))
+            {
+                return 2 * SteinGCD(firstNumber / 2, secondNumber / 2);
+            }
+            if ((firstNumber % 2 == 0) && (secondNumber % 2 != 0))
+            {
+                return SteinGCD(firstNumber / 2, secondNumber);
+            }
+            if ((firstNumber % 2 != 0) && (secondNumber % 2 == 0))
+            {
+                return SteinGCD(firstNumber, secondNumber / 2);
+            }
+            return SteinGCD(secondNumber, Math.Abs(firstNumber - secondNumber));
+        }
+
+    }
+}
---
+++ MathNewton/NewtonMethod.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+namespace MathNewton
+{
+    public class NewtonMethod
+    {
+        /// <summary>
+        /// This method is the root of a certain degree of number.
+        /// </summary>
+        /// <param name="number">the number of which is the degree of.</param>
+        /// <param name="power">root level.</param>
+        /// <param name="e">the accuracy of determining the number of.</param>
+        /// <returns>root of number./returns>
+        public static double Sqrt(double number, int power, double e)
+        {
+            double rootPref = 0;
+            double rootNext = 1;
+
+            if (number <= 0 && power%2!=0 || e <= 0 || e > 1)
+            {
+                throw new Exception("Invalid function arguments!");
+            }
+
+            while (Math.Abs(rootNext - rootPref) >= e)
+            {
+                rootPref = rootNext;
+                rootNext = (1.0 / power) * ((power - 1) * rootPref + number / Math.Pow(rootPref, power - 1));
+
+            }
+
+            return rootNext;
+        }
+
+    }
+}
